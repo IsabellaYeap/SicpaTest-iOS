9928dcecd7321f76fd747620dad3458b8b8bbb2b
Isabella <yeap.pei.ting@gmail.com>
2022-02-27 08:48:25 +0800

    Initial Commit

---
+++ SicpaTest22UITests/SicpaTest22UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SicpaTest22UITestsLaunchTests.swift
+//  SicpaTest22UITests
+//
+//  Created by Yeap Pei Ting on 27/02/2022.
+//
+
+import XCTest
+
+class SicpaTest22UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
